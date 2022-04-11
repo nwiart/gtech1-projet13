18fbfe530c1daf0b729e70c75c1bcf62b9e6e57f
nwiart <nwiart@gaming.tech>
2022-04-11 17:47:20 +0200

    React routes, home, header and footer

---
+++ app/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default class Home extends React.Component {
+	
+	render() {
+
+		return (
+			<>
+				<Header />
+
+				<Footer />
+			</>
+		);
+	}
+}
