dfbf8e903df324791a2ee55d9d7d7b270a7249d9
zbie <ejoachimgabin@gaming.tech>
2022-04-21 11:41:15 +0200

    data accueil

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -11,8 +11,10 @@ import calquelogo from "../images/CalqueLogo.png";
 import ligne from "../images/Ligne22.png";
 import ligne_blanche from "../images/Ligne30.png";
 import carte from "../images/carte_de_fronce.png";
-import plan1 from "../images/arrierePlan1.png";
 import plan2 from "../images/arrierePlan2.png";
+import plan_top from "../img/product_background_top.png";
+import plan_bottom from '../img/product_background_bottom.png';
+
 import image_footer from "../images/ImageFooter.png";
 import boulanger from "../images/devenir-boulang.png";
 import boulanger1 from "../images/ImageBoulanger1.png";
@@ -24,14 +26,26 @@ import carousel2 from "../images/carousel2.png";
 import carousel3 from "../images/carousel3.png";
 
 
+const responsive = {
+    0: { items: 1 },
+    568: { items: 2 },
+    1024: { items: 3 },
+};
 
-	const handleDragStart = (e) => e.preventDefault();
+const items = [
+    <div className="item" data-value="boulanger1">1</div>,
+    <div className="item" data-value="boulanger2">2</div>,
+    <div className="item" data-value="boulanger3">3</div>,
+];
 
-	const items = [
-	<img src="boulanger1" onDragStart={handleDragStart} />,
-	<img src="boulanger2" onDragStart={handleDragStart} />,
-	<img src="boulanger3" onDragStart={handleDragStart} />,
-	];	
+const Alice = () => (
+    <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+    />
+);
 
 export default class Home extends React.Component {
 
@@ -42,7 +56,7 @@ export default class Home extends React.Component {
 		return (
 			<>
 				<Header/>
-				<Container style={{ background: "white", minHeight: "100vh" }}>
+				<Container style={{ background: "white", color: "black", minHeight: "100vh" }}>
 				<div class="container">
 					<div className="text-center">
 						<div className="logoTitre">
@@ -50,18 +64,20 @@ export default class Home extends React.Component {
 						</div>
 
 						<div className="blue">
-							<div style={{ display: 'block', width: 700, padding: 30 }}>
-								<Carousel>
-									<Carousel.Item interval={1500}>
-										<img className="d-block w-100" src={carousel1} alt="Image One"/>
-									</Carousel.Item>
-									<Carousel.Item interval={1500}>
-										<img className="d-block w-100" src={carousel2} alt="Image Two"/>
-									</Carousel.Item>
-									<Carousel.Item interval={1500}>
-										<img className="d-block w-100" src={carousel3} alt="Image Three"/>
-									</Carousel.Item>
-								</Carousel>
+							<div className="carouselle">								
+								<div style={{ display: 'block', width: 700, padding: 30 }}>
+									<Carousel>
+										<Carousel.Item interval={1500}>
+											<img className="d-block w-100" src={carousel1} alt="Image One"/>
+										</Carousel.Item>
+										<Carousel.Item interval={1500}>
+											<img className="d-block w-100" src={carousel2} alt="Image Two"/>
+										</Carousel.Item>
+										<Carousel.Item interval={1500}>
+											<img className="d-block w-100" src={carousel3} alt="Image Three"/>
+										</Carousel.Item>
+									</Carousel>
+								</div>
 							</div>
 							<img src={ligne}/>
 							<div className="text-description">
@@ -80,35 +96,39 @@ export default class Home extends React.Component {
 								<h2>Découvrez les produit de nos régions</h2>
 							</div>
 							<img src={carte} width="572"/>
-
+							<br></br>
 							<img src={ligne}/>
-							<img src={plan1} width="1138"/>
 						</div>
-						<div>
-							<h3>Nos recommandations produits</h3>
-						</div>
-						<img src={ligne_blanche}/>
 
-						
-						<img src={ligne_blanche}/>
-						<img src={plan1} width="1138"/>
-						<img src={plan2} width="1138"/>
-						<div>
-							<h3>Nos recommandations artisans</h3>
-						</div>
-						<Container style={{ background: "white", padding: "12px" }}>
-							<Col xs={6}>
+						<div className="second_backround">
+							<img src={plan_top} width="1138"/>
+							<div>
+								<h3>Nos recommandations produits</h3>
+							</div>
+							<img src={ligne_blanche}/>
 
-								<img className="img-fluid" src={boulanger} />
-								<AliceCarousel mouseTracking items={items} />
-							</Col>
-						</Container>
+							
+							<img src={ligne_blanche}/>
+							<img src={plan_bottom} width="1138"/>
+							
+							<div>
+								<h3>Nos recommandations artisans</h3>
+							</div>
+							<Container style={{ background: "white", padding: "12px" }}>
+								<Col xs={6}>
+
+									<img className="img-fluid" src={boulanger} />
+									<AliceCarousel mouseTracking items={items} />
+								</Col>
+							</Container>
+							<img src={plan2} width="1138"/>
+						</div>
 					</div>
 				</div>
 				</Container>
 				<Footer />
 				<div className="end_page">
-				<img src={image_footer} width="100%"/>
+					<img src={image_footer} width="100%"/>
 				</div>
 			</>
 		);
